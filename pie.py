541ea19cfa043937e137b42db4072563b351dfda
gabrielpictet <gabriel@marsillon.ch>
2025-03-21 12:16:24 +0100

    Added comments

--- pie.py
+++ pie.py
@@ -1,13 +1,17 @@
 #make a pie chart comparing number of volunteers with number of paid staff
+# import the necessary libraries
 import matplotlib
 import matplotlib.pyplot as plt
-# load dataframe individual.csv
 import pandas as pd
+
+# load dataframe individual.csv
 df = pd.read_csv("individual.csv")
+
 # count the number of volunteers
 volunteers = df["Volunteer"].sum()
 # count the number of paid staff
 paid_staff = df["Staff"].sum()
+
 # put the data into a list
 data = [volunteers, paid_staff]
 
